59cbbad5dfa340027f0e880d8250005e9fd8fac0
kryfo <superdkk@gmail.com>
2018-06-20 17:18:40 +0800

    注释完 qualoth

--- qualoth/config.py
+++ qualoth/config.py
@@ -11,7 +11,7 @@ reload(sys)
 from MayaPlugin import PluginBase
 
 
-class SoupConfig():
+class QualothConfig():
     def __init__(self,clientInfo):
         # self.MyLog(clientInfo)
         self.cgName = clientInfo['cgName']
@@ -25,23 +25,24 @@ class SoupConfig():
         self._PluginDir = os.path.join(self.Base.get_json_ini('Node_D'),self.pluginName,'software',self.cgName + self.cgVersion + '_' + self.pluginName + self.pluginVersion).replace('\\','/')
         self.MyLog(self._PluginDir)
                 
-    def MyLog(self,message,extr="SoupSetup"):      
+    def MyLog(self,message,extr="qualothSetup"):      
         if str(message).strip() != "":
             print("[%s] %s"%(extr,str(message)))
 
     def setEnv(self):
-        #删除本地c盘残留的旧插件
-        if os.path.exists(r"C:\Program Files\Autodesk\Maya2016\bin\plug-ins\SOuP.mll"):
-            os.system(r'del /s /q "C:\Program Files\Autodesk\Maya2016\bin\plug-ins\SOuP.mll"')
-        #添加环境变量
+        #设置环境变量
+        _PATH_ENV = os.environ.get('PATH')
         _MAYA_PLUG_IN_PATH  = os.environ.get('MAYA_PLUG_IN_PATH')
-        os.environ['MAYA_PLUG_IN_PATH'] = (_MAYA_PLUG_IN_PATH + r";" if _MAYA_PLUG_IN_PATH else "") + self._PluginDir + r"/plug-ins"
+        _MAYA_SCRIPT_PATH=os.environ.get('MAYA_SCRIPT_PATH')
+        os.environ['PATH'] = (_PATH_ENV + r";" if _PATH_ENV else "") + self._PluginDir + r"\bin"
+        os.environ['MAYA_PLUG_IN_PATH'] = (_MAYA_PLUG_IN_PATH + r";" if _MAYA_PLUG_IN_PATH else "") + self._PluginDir + r"\bin"
+        os.environ['MAYA_SCRIPT_PATH'] = (_MAYA_SCRIPT_PATH if _MAYA_SCRIPT_PATH else "") + r";"+ self._PluginDir + r"\script" 
 
 def main(*args):
     infoDict = args[0]
-    configPlugin = SoupConfig(infoDict)
+    configPlugin = QualothConfig(infoDict)
     configPlugin.setEnv()
-    configPlugin.MyLog( "set Soup env finish")
+    configPlugin.MyLog( "set Ornatrix env finish")
 
 
 if __name__ == '__main__':
